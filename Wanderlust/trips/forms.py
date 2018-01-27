d2defcb2f1402ceeccedf15debc0ffb54680e6f0
Aly-Reda <aly.mohammed.reda@gmail.com>
2018-01-27 17:14:55 +0200

    "Aly"

--- Wanderlust/trips/forms.py
+++ Wanderlust/trips/forms.py
@@ -9,9 +9,8 @@ class CreditCardField(forms.IntegerField):
 
 class SignUpForm(UserCreationForm):
     email = forms.CharField(max_length = 30, required = True, widget= forms.EmailInput())
-    credit_card_no = CreditCardField(forms.Form, required=True, label= "Credit Card")
+    #credit_card_no = CreditCardField(forms.Form, required=True, label= "Credit Card")
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'credit_card_no')
-
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
--- Wanderlust/trips/models.py
+++ Wanderlust/trips/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.urlresolvers import reverse
 from django.forms import ModelForm, forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
